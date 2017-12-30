95bed6ca2ea6ff551db2e9ec74a3004df1eacf1c
leotangram <leotangram@gmail.com>
2017-12-29 20:05:46 -0500

    Implementa el saludo al iniciar sesión... esperando cambiarlo al nombre en lugar del correo

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171229045449) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171229045449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
