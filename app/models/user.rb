80968210a62308fb97761afd5a84927f1f6e111d
leotangram <leotangram@gmail.com>
2017-12-30 22:44:07 -0500

    Oculta editar y eliminar producto si el usuario no está autenticado

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@
 #  id                     :integer          not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  name                   :string           default(""), not null
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
